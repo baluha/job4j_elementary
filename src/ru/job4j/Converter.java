f07041b744b78158e3157d536ccce61f79c0fa14
baluha <romanbalushka@gmail.com>
2020-10-15 21:24:25 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -3,6 +3,7 @@ package ru.job4j;
 public class Converter {
     public static int rubleToEuro(int value){
         int rsl = value/70;
+        return rsl;
     }
     public  static int rubleToDollar(int value){
         int rsl = value/60;
