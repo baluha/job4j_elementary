f884d4f413bc44bbe9bf2586cffceb00f392f07b
baluha <romanbalushka@gmail.com>
2020-10-15 20:58:08 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/Treinee.java
+++ src/ru/job4j/Treinee.java
@@ -1,13 +1,13 @@
 package ru.job4j;
 
 public class Treinee {
-    public class Argmethod{
+
         public static void hello(String name){
             System.out.println("Hello, " + name);
         }
         public static void main(String[]args){
             String name = "Vasilii Vasev";
-            Argmethod.hello(name);
+            Treinee.hello(name);
         }
     }
-}
+
---
+++ src/ru/job4j/ru/job4j/calculator/task41.java
@@ -0,0 +1,17 @@
+package ru.job4j.ru.job4j.calculator;
+
+public class task41 {public static void hello(String name) {
+    System.out.println("Hello, " + name);
+}
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+
+
+
+        task41.hello(name);
+
+
+    }
+}
