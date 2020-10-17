129572168e0394f22bf821fec7394408e30b91bf
baluha <romanbalushka@gmail.com>
2020-10-17 20:22:36 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/Variables.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args)
+    {
+        int age = 18;
+        float lenght = 78.0332f;
+        boolean baba = true;
+    }
+}
