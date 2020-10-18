e06f2fa0a62ee4fa1ed1be7e0726b25acfa214f4
baluha <romanbalushka@gmail.com>
2020-10-18 17:26:11 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/Idea.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Idea {
+    public static void main(String[] args) {
+String idea = "I like java!";
+        System.out.println(idea);
+        System.out.println(idea + " But i am a newbie.");
+        int year = 2020;
+        System.out.println(idea + " " + year);
+    }
+}
