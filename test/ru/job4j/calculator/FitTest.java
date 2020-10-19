b5b68f2bb7c955f070e86f18ee2978f7c34f2441
baluha <romanbalushka@gmail.com>
2020-10-19 23:32:28 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+    }
+
+    @Test
+    public void womanWeight() {
+    }
+}
