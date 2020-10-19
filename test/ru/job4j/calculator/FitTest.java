8292f58e41875c03ef92c9b7d7e8e040073264d2
baluha <romanbalushka@gmail.com>
2020-10-19 23:44:09 +0300

    3.1. Загрузка файлов в github. [#243562]

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -6,10 +6,19 @@ import org.junit.Test;
 public class FitTest {
 
     @Test
-    public void manWeight() {
+    public void whenMan187Then100() {
+        short in = 187;
+        double expected = 100.05;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+
     }
 
     @Test
-    public void womanWeight() {
+    public void whenWoman187Then88() {
+        short in = 187;
+        double expected = 88.55;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
