39fdc6ca1192118f83a5839f33fd132cd7761d3c
baluha <romanbalushka@gmail.com>
2020-10-20 01:00:33 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -8,6 +8,6 @@ public class SqArea {
 
     public static void main(String[] args) {
         double result1 = SqArea.square(12, 2);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+        System.out.println(" p = 12, k = 2, s = 1, real = " + result1);
     }
 }
