291b7d185fa2679a58f855734c23201bd4e19e5a
baluha <romanbalushka@gmail.com>
2020-10-20 18:48:15 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,14 +1,14 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int maxint (int left, int right) {
+    public static int max (int left, int right) {
         int point = left > right ? left : right;
         return point;
 
     }
 
     public static void main(String[] args) {
-        int maximum = Max.maxint(2, 1);
+        int maximum = Max.max(2, 1);
         System.out.println(maximum);
     }
 }
