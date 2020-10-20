aa42540db98006d3cba79352ce2bc538c0aabe7c
baluha <romanbalushka@gmail.com>
2020-10-20 18:29:23 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/Oiu.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+import java.lang.Math;
+
+public class Oiu {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int result = Math.max((Math.max(a, b)), c);
+
+        System.out.println(result);
+    }
+}
