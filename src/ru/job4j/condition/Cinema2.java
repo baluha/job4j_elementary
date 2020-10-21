f825c32491ed6a527e87672b1260cf0ff46e54c7
baluha <romanbalushka@gmail.com>
2020-10-21 18:14:53 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/condition/Cinema2.java
+++ src/ru/job4j/condition/Cinema2.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Cinema2 {
     public static void permission(boolean allowByParent, boolean hasMoney) {
-        if (allowByParent == true && hasMoney == true) {
+        if (allowByParent && hasMoney) {
 
             System.out.println("I can go to the cinema");
         } else {
