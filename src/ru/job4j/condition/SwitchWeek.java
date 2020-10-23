a26045395793f0abc7409fa63d0526d064ae3443
baluha <romanbalushka@gmail.com>
2020-10-24 02:13:54 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/condition/SwitchWeek.java
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -5,18 +5,28 @@ public class SwitchWeek {public String nameOfDay(int day) {
     switch(day) {
     case 1 :
         name = "понедельник";
+        break;
         case 2 :
             name = "вторник";
+            break;
         case 3 :
             name = "среда";
+            break;
         case 4 :
             name = "четверг";
+            break;
         case 5 :
             name = "пятница";
+            break;
         case 6 :
             name = "суббота";
+            break;
         case 7 :
             name = "воскресение";
+            break;
+        default:
+            name = "Неттакогодня";
+            break;
         return name;
 }
 
