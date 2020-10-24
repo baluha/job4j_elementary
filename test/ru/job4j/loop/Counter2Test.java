4d6f41578af36ca450f7e0687c715933280f46a0
baluha <romanbalushka@gmail.com>
2020-10-24 21:48:25 +0300

    3.1. Загрузка файлов в github. [#243562]

--- test/ru/job4j/loop/Counter2Test.java
+++ test/ru/job4j/loop/Counter2Test.java
@@ -8,9 +8,23 @@ import static org.junit.Assert.assertThat;
 public class Counter2Test {
 
     @Test
+    public void whenSumEvenNumbersFromThreeToEightThenEighteen() {
+        int rsl = Counter2.sumByEven(3, 8);
+        int expected = 18;
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
         int rsl = Counter2.sumByEven(1, 10);
         int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromOneToOneThenZero() {
+        int rsl = Counter2.sumByEven(1, 1);
+        int expected = 0;
         assertThat(rsl, is(expected));
     }
 }
