6f5412bdfffa92ebb7f548276613894769aab991
baluha <romanbalushka@gmail.com>
2020-10-24 21:48:57 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/loop/Counter2.java
+++ src/ru/job4j/loop/Counter2.java
@@ -1,16 +1,6 @@
 package ru.job4j.loop;
 
 public class Counter2 {
-    public static int sum(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-
-            if (i % 2 == 0) {
-                sum += i;
-            }
-        }
-        return sum;
-    }
     public static int sumByEven(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++ ) {
@@ -22,10 +12,6 @@ public class Counter2 {
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-
         System.out.println(sumByEven(0, 10));
         System.out.println(sumByEven(3, 8));
         System.out.println(sumByEven(1, 1));
