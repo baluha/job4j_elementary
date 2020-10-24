7cbb8b3b5c87ebe99a36e988d5de272a60e61b19
baluha <romanbalushka@gmail.com>
2020-10-24 18:30:21 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -3,8 +3,9 @@ package ru.job4j.condition;
 public class MultiMax {
     public static int max(int first, int second, int third) {
 
-        int result = first > second ? (first > third ?  first : third) : (second > third ?  second : third);
-        return  result;
+        int result = first > second ? first : second;
+
+        return result > third ? result : third;
 
     }
 }
