fce0cdb526b54cc86706047c3738ba6932b008fb
baluha <romanbalushka@gmail.com>
2020-10-25 16:50:43 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/loop/Factorial.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        for (int i=1; i<=n; i++ ){
+            if ()
+        }
+
+
+        int result = 0;
+        return result;
+    }
+}
