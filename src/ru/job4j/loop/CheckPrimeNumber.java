b8d23b2d74bd2d841f1d79d7da526be690c42f14
baluha <romanbalushka@gmail.com>
2020-10-27 23:37:42 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,15 +2,14 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-
-        if (((number % 2 == 0) || (number % 3 == 0) || (number % 5 == 0) || (number % 7 == 0))) {
-            return false;
+boolean prime = number > 1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
+            }
+                break;
         }
-        else  if ((number == 2) || (number == 3) || (number == 5) || (number == 7)) {
-            return true;
-        }
-        else return true;
-        }
-
+        return prime;
     }
+}
 
