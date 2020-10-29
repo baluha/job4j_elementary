9b74324fa3f3b19bde692d7bb642732ee8f9e463
baluha <romanbalushka@gmail.com>
2020-10-30 02:03:30 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -13,9 +13,10 @@ public class ArrayDefinition {
         names[1] = "Petr Lozovoi";
         names[2] = "Arhebald Kolesnik";
         names[3] = "Aristarh Bukin";
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
-
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
         }
 
     }
