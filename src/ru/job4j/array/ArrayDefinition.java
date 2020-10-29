2cb30b9dc5c9634879ca54b19e63272abac34274
baluha <romanbalushka@gmail.com>
2020-10-30 01:59:07 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Vasia Vasev";
+        names[1] = "Petr Lozovoi";
+        names[2] = "Arhebald Kolesnik";
+        names[3] = "Aristarh Bukin";
+
+
+
     }
 }
