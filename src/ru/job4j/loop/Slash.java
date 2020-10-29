66d2d3cda05e78dc7e00ca321743e9d1bb635c42
baluha <romanbalushka@gmail.com>
2020-10-29 23:04:48 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -4,7 +4,11 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                if (row == cell || row == size - cell - 1) {
+                boolean left = row == cell; /* добавить условие, по которому нужно определить ставить ли символ или нет. */
+                boolean right = row + cell == size - 1; /* добавить условие, что нужно ставить элемент в правый угол. */
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
                     System.out.print("0");
                 } else {
                     System.out.print(" ");
@@ -14,6 +18,7 @@ public class Slash {
         }
         System.out.println();
     }
+
 
     public static void main(String[] args) {
         System.out.println("Draw by 3");
