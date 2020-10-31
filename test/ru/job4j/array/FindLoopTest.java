a18eef2942e2ed2c52a117809551920fbfe69858
baluha <romanbalushka@gmail.com>
2020-10-31 22:17:46 +0300

    3.1. Загрузка файлов в github. [#243562]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -8,7 +8,7 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas5Then0() {
-        int input[] = {5, 4, 3, 2};
+        int[] input = {5, 4, 3, 2};
         int value = 5;
         int result = FindLoop.indexOf(input, value);
         int expect = 0;
@@ -16,7 +16,7 @@ public class FindLoopTest {
     }
 
     public void whenArrayHas5Then1() {
-        int input[] = {4, 5, 4, 3, 2};
+        int[] input = {4, 5, 4, 3, 2};
         int value = 8;
         int result = FindLoop.indexOf(input, value);
         int expect = -1;
