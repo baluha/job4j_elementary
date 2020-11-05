21d94affc5db254f2c816be0a9db3f85a71bdeef
baluha <romanbalushka@gmail.com>
2020-11-05 03:58:28 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/array/Min.java
+++ src/ru/job4j/array/Min.java
@@ -3,11 +3,9 @@ package ru.job4j.array;
 public class Min {
     public static int findMin(int[] array) {
         int min = array[0];
-        int minIndex = 0;
         for (int index = 0; index < array.length; index++) {
             if (array[index] < min) {
                 min = array[index];
-                minIndex = index;
             }
         }
         return min;
