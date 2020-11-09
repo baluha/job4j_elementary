2956aa0accb486fe829b4871e1ba60cb7bf14fc5
baluha <romanbalushka@gmail.com>
2020-11-09 18:29:12 +0300

    6.7.1. Моно строка в матрице. [#390422]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int i = post.length - 1, j = word.length - 1; i >= 0; i--, j--) {
-            if (word[j] != post[i]) {
+        for (int i = 0 ; i < post.length - 1; i++) {
+            if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
                 result = false;
                 break;
             }
