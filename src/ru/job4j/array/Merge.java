27154a7d612e0b2fd429ab04f2666ebf43d3006d
Администратор <xatowar@gmail.com>
2020-12-12 19:02:46 +0300

    Merge

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -5,7 +5,7 @@ public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         System.arraycopy(left, 0, rsl,  0, left.length);
-        System.arraycopy(right,  0, rsl, left.length, right.length);
+        System.arraycopy(right, 0, rsl, left.length, right.length);
         SortSelected.sort(rsl);
         return rsl;
     }
