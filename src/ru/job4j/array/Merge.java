d59d9290570449b44ab058d578838071a0dcbc4e
Администратор <xatowar@gmail.com>
2020-12-13 13:15:54 +0300

    Merge

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -4,24 +4,20 @@ public class Merge {
 
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i = 0, r = 0, l = 0; i < rsl.length; i++) {
-            if (r == right.length) {
-                rsl[i] = left[l];
-                l++;
-                continue;
-            } else if (l == left.length) {
-                rsl[i] = right[r];
-                r++;
-                continue;
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < rsl.length;) {
+            while (l != left.length && r != right.length) {
+                rsl[i++] = left[l] < right[r] ? left[l++] : right[r++];
             }
-            if (left[l] <= right[r]) {
-                rsl[i] = left[l];
-                l++;
+            if (l == left.length) {
+                rsl[i++] = right[r++];
+
             } else {
-                rsl[i] = right[r];
-                r++;
+                rsl[i++] = left[l++];
             }
         }
         return rsl;
-    }
-}
+        }
+
+        }
